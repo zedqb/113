f310e1af24e575098c3e60e5ea2f1ffa3b2a9a8b
nitro <zedrjuzge@gmail.com>
2025-05-19 22:18:21 +0300

    init

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl(userDao);
+
+        userService.saveUser("Миша", "Журиков", (byte) 25);
+        userService.saveUser("Вадим", "Старостин", (byte) 33);
+        userService.saveUser("Миша", "Иванков", (byte) 32);
+        userService.saveUser("Вин", "Дизель", (byte) 63);
     }
 }
+
+
+
+
+
+
+
+
+
